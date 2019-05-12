0921447c0f4d7137eca86ffb4433fdd81765e629
zack <zack.bitcoin@gmail.com>
2019-05-12 09:58:16 -0400

    display prices better

--- js/oracle_list.js
+++ js/oracle_list.js
@@ -53,20 +53,39 @@
         } else {
             var h = l[0];
             var t = document.createElement("div");
-            var direction;
-            if (h[4] == 2) {
-                direction = "true/long/stablecoin";
+            var direction, type;
+            if (h[9] == 1) {
+                type = "binary";
+                price = h[3];
+                return display_offers2(l, h, t, direction, type, price, " or ", "");
+            } else if (h[9] == 2) {
+                var oid = h[2];
+                type = "scalar"
+                oracle_limit(oid, function(oracle_max) {
+                    console.log("oracle_list callback");
+                    console.log(oracle_max);
+                    price = (1023 - h[3]) * oracle_max / 1023;
+                    return display_offers2(l, h, t, direction, type, price, " stablecoin/veo or ", " veo/stablecoin;");
+                });
             } else {
-                direction = "false/short/long-veo";
+                console.log(h[9]);
+                console.log("contract type not supported.");
             }
-            var text = "price = ".concat(h[3]).concat("; you win if the result is ").concat(direction).concat("; they pay = ").concat(s2c(h[7])).concat("; you pay = ").concat(s2c(h[8])).concat("; expires: ").concat(h[5]);
-            t.innerHTML = text;
-            offers.appendChild(t);
-            var button = button_maker2("display this contract", function() { display_contract(h[1]) });
-            offers.appendChild(button);
-            offers.appendChild(br());
-            display_offers(l.slice(1));
         }
+    };
+    function display_offers2(l, h, t, direction, type, price, d1message, d2message) {
+        if (h[4] == 2) {
+            direction = "true/long/stablecoin";
+        } else {
+            direction = "false/short/long-veo";
+        }
+        var text = "bet type: ".concat(type).concat("; price = ").concat(price).concat(d1message).concat(1/price).concat(d2message).concat(" you win if the result is ").concat(direction).concat("; they pay = ").concat(s2c(h[7])).concat("; you pay = ").concat(s2c(h[8])).concat("; expires: ").concat(h[5]);
+        t.innerHTML = text;
+        offers.appendChild(t);
+        var button = button_maker2("display this contract", function() { display_contract(h[1]) });
+        offers.appendChild(button);
+        offers.appendChild(br());
+        display_offers(l.slice(1));
     };
     function display_contract(CID) {
         variable_public_get(["get_offer_contract", CID], function(C) {
