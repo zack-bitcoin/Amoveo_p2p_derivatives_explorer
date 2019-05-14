b93432910205cb69b474de12f68ca768fa123c8c
zack <zack.bitcoin@gmail.com>
2019-05-14 02:25:14 -0400

    fix typo

--- js/oracle_list.js
+++ js/oracle_list.js
@@ -30,8 +30,8 @@
             return 0;
         } else {
             var h = l[0];
-            //variable_public_get(["oracle", h[1]], function(Oracle) {
-            variable_public_get(["oracle", h], function(Oracle) {
+            variable_public_get(["oracle", h[1]], function(Oracle) {
+            //variable_public_get(["oracle", h], function(Oracle) {
                 console.log(JSON.stringify(Oracle[1]));
                 var button = button_maker2(atob(Oracle[1][4]), function() { return display_oracle(Oracle[1][2], Oracle[1][3]) });
                 div.appendChild(button);
