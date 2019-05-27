25453d6ac53cea62034d12d0c145411e33b9facb
zack <zack.bitcoin@gmail.com>
2019-05-27 18:38:14 -0400

    seperate the button from the text for readability.

--- js/oracle_list.js
+++ js/oracle_list.js
@@ -34,8 +34,10 @@
             variable_public_get(["oracle", h[1]], function(Oracle) {
             //variable_public_get(["oracle", h], function(Oracle) {
                 console.log(JSON.stringify(Oracle));
-                var button = button_maker2(atob(Oracle[1][4]), function() { return display_oracle(Oracle[1][2], Oracle[1][3]) });
+                var t = text(atob(Oracle[1][4]));
+                var button = button_maker2("display trades", function() { return display_oracle(Oracle[1][2], Oracle[1][3]) });
                 div.appendChild(button);
+                div.appendChild(t);
                 div.appendChild(br());
                 display_oracles(l.slice(1));
             });
