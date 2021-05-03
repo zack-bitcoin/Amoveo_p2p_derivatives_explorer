3e7b722cf3a3da33e197e0be71d26b06e6fdd5c8
zack <zack.bitcoin@gmail.com>
2021-05-03 11:07:33 -0400

    switching back to old version of cowboy

--- apps/amoveo_p2p_derivatives_explorer/src/http_handler.erl
+++ apps/amoveo_p2p_derivatives_explorer/src/http_handler.erl
@@ -1,20 +1,26 @@
 -module(http_handler).
--export([init/3, handle/2, terminate/3, doit/1]).
+-export([init/3, handle/2, terminate/3, doit/1, init/2]).
 -include("records.hrl").
+init(Req0, Opts) ->
+    handle(Req0, Opts).	
 init(_Type, Req, _Opts) -> {ok, Req, no_state}.
 terminate(_Reason, _Req, _State) -> ok.
 handle(Req, State) ->
     {ok, Data0, Req2} = cowboy_req:body(Req),
-    {{IP, _}, Req3} = cowboy_req:peer(Req2),
+    %{{IP, _}, Req3} = cowboy_req:peer(Req2),
+    {IP, _} = cowboy_req:peer(Req2),
     %io:fwrite("http handler got message: "),
     %io:fwrite(Data0),
     %io:fwrite("\n"),
     Data1 = jiffy:decode(Data0),
     Data = packer:unpack_helper(Data1),
     D = packer:pack(doit(Data)),
+    %Headers = #{ <<"content-type">> => <<"application/octet-stream">>,
+%	       <<"Access-Control-Allow-Origin">> => <<"*">>},
     Headers=[{<<"content-type">>,<<"application/octet-stream">>},
-	     {<<"Access-Control-Allow-Origin">>, <<"*">>}],
-    {ok, Req4} = cowboy_req:reply(200, Headers, D, Req3),
+    {<<"Access-Control-Allow-Origin">>, <<"*">>}],
+    %{ok, Req4} = cowboy_req:reply(200, Headers, D, Req2),
+    Req4 = cowboy_req:reply(200, Headers, D, Req2),
     {ok, Req4, State}.
 doit({test}) -> {ok, "success"};
 
